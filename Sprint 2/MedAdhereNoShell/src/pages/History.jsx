3b9cce6df3707712868a1c3e54c59d54f18641cb
qbrentz <qbrentz@vcu.edu>
2025-04-09 22:09:14 -0400

    IT COMPILES

---
+++ Sprint 2/MedAdhereNoShell/src/pages/History.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { Container, Table } from "react-bootstrap";
+import Navbar from "../components/Navbar";
+import axios from "axios";
+
+export default function History() {
+  const username = localStorage.getItem("username");
+  const [logs, setLogs] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/api/logs/username/${username}`)
+      .then((res) => setLogs(res.data))
+      .catch((err) => console.error("Failed to load logs", err));
+  }, [username]);
+
+  return (
+    <>
+      <Navbar />
+      <Container className="mt-4">
+        <h3>Medication History</h3>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Medication</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {logs.map((log) => (
+              <tr key={log.id}>
+                <td>{log.timestamp}</td>
+                <td>{log.medName}</td>
+                <td>{log.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
+    </>
+  );
+}
